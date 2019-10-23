2420aa1bf8bb593866531db410b699d70a4ece4c
Vasco Veloso <vasco@codenomads.nl>
2019-10-23 13:32:32 +0200

    Experimenting with thread affinity.

--- src/main/kotlin/nl/codenomads/workshop/app.kt
+++ src/main/kotlin/nl/codenomads/workshop/app.kt
@@ -1,5 +1,17 @@
 package nl.codenomads.workshop
 
-fun main() {
-    println("Hello world")
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+
+fun main() = runBlocking {
+    val log = LoggerFactory.getLogger("main")
+    for(i in 0..10) {
+        launch {
+            log.info("before delay in {}", i)
+            delay(1000)
+            log.info("after delay in {}", i)
+        }
+    }
 }
