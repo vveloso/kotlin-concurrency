614568c851d5f640959f438314b819fa7c389c4a
Vasco Veloso <vasco@codenomads.nl>
2019-11-13 14:03:43 +0100

    Basis for experimenting with flows.

--- src/main/kotlin/nl/codenomads/workshop/app.kt
+++ src/main/kotlin/nl/codenomads/workshop/app.kt
@@ -1,5 +1,13 @@
 package nl.codenomads.workshop
 
+/*
+
+  Challenge:
+
+  Build a Flow that provides ten items retrieved from an API using the HTTP client from before.
+
+ */
+
 fun main() {
     println("Hello world")
 }
