3322799a4e0fe88af4bfb7698daa901e3a9b9152
Vasco Veloso <vasco@codenomads.nl>
2020-03-04 15:59:09 +0100

    Context documentation.

--- src/main/kotlin/nl/codenomads/workshop/app.kt
+++ src/main/kotlin/nl/codenomads/workshop/app.kt
@@ -3,7 +3,19 @@ package nl.codenomads.workshop
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
+/*
+ Coroutines have been created to allow explicit concurrency without much overhead.
+ Therefore several constructs were written to facilitate this purpose.
+
+ runBlocking() and launch() are two of those constructs. The first creates a coroutine
+ context and does not return until all coroutines started within complete. The latter
+ launches a coroutine concurrently in a different context but does not block or suspend
+ the caller.
+
+ Can you reason about the sequence in which the two strings appear in the console?
+ What would happen if runBlocking() would not block? Why?
+ */
+fun main(): Unit = runBlocking {
     launch {
         println("Hello")
     }
