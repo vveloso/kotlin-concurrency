b27872efa2535fbb2e32452b6b9a142aaa358ae0
Vasco Veloso <vasco@codenomads.nl>
2020-03-04 16:09:59 +0100

    Exercise description.

--- src/main/kotlin/nl/codenomads/workshop/app.kt
+++ src/main/kotlin/nl/codenomads/workshop/app.kt
@@ -2,6 +2,15 @@ package nl.codenomads.workshop
 
 import kotlin.concurrent.thread
 
+/*
+ This exercise has two parts:
+    1. Launch 100000 threads with the loop below.
+    2. Refactor the main() function to launch 100000 coroutines that also just "sleep"
+       for one second each.
+       Hint: remember runBlocking(), launch() and delay().
+
+ Are there differences in results between the two approaches? Why?
+ */
 fun main() {
     var counter = 0
     try {
