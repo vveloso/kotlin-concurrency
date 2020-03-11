6bd83f4074effdcdcc8c3f5c97db1da8f0519df6
Vasco Veloso <vasco@codenomads.nl>
2020-03-11 14:25:21 +0100

    Exercise description.

--- src/main/kotlin/nl/codenomads/workshop/app.kt
+++ src/main/kotlin/nl/codenomads/workshop/app.kt
@@ -8,6 +8,18 @@ import kotlinx.serialization.Serializable
 import org.apache.commons.text.StringEscapeUtils
 import kotlin.system.measureTimeMillis
 
+/**
+ * Explicit concurrency.
+ *
+ * 1. Measure the time it takes to get one translation of hello in one language.
+ *
+ * 2. Get the translation for at least four languages, print them while measuring the time again.
+ *
+ * 3. Issue the requests concurrently (hint: async()). Still measuring the time.
+ *
+ * 4. Did the third version run faster?
+ */
+
 @Serializable
 data class HelloTranslation(val code: String, val hello: String)
 
