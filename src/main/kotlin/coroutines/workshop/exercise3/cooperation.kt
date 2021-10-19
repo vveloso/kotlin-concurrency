7d08aed2561f72616a104b38ddce31f277ccac80
Vasco Veloso <vveloso@gmail.com>
2021-10-19 19:56:29 +0200

    Proposed solution for exercise 3.

--- src/main/kotlin/coroutines/workshop/exercise3/cooperation.kt
+++ src/main/kotlin/coroutines/workshop/exercise3/cooperation.kt
@@ -1,8 +1,11 @@
 package coroutines.workshop.exercise3
 
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 /*
@@ -30,35 +33,75 @@ fun main() {
 
     warmUp() // this is so the JIT compiler will kick in, and we get more accurate measurements.
 
-    val duration = measureTimeMillis {
-        calculatePi()
+    val linearDuration = measureTimeMillis {
+        calculatePiLinearly()
+    }
+    val threadsDuration = measureTimeMillis {
+        calculatePiWithThreads()
+    }
+    val coroutinesDuration = measureTimeMillis {
+        calculatePiWithCoroutines()
     }
 
-    println("Calculation took $duration ms")
+    println("(linear)     Calculation took $linearDuration ms")
+    println("(threads)    Calculation took $threadsDuration ms")
+    println("(coroutines) Calculation took $coroutinesDuration ms")
 }
 
-fun calculatePi() = runBlocking {
+fun calculatePiLinearly() = runBlocking {
     // approximate pi up to 9 digits: up to 1_000_000_000 terms
-    val pi = sumLeibniz(0, 1_000_000_000) * 4
+    val pi = sumLeibniz(0, 999_999_999) * 4
+    println(pi) // (correct value of pi to 16 digits: 3.1415926535897932)
+}
+
+fun calculatePiWithThreads() {
+    // approximate pi up to 9 digits: up to 1_000_000_000 terms
+    val termCount = 1_000_000_000L
+    val iterations = 10
+    val interval = termCount / iterations
+
+    val intermediateResults = DoubleArray(iterations)
+    val threads = Array(iterations) {
+        thread { intermediateResults[it] = runBlocking { sumLeibniz(it * interval, (it + 1) * interval - 1) } }
+    }
+    threads.forEach { it.join() }
+    val pi = intermediateResults.sum() * 4
 
     println(pi) // (correct value of pi to 16 digits: 3.1415926535897932)
 }
 
-fun sumLeibniz(start: Long, end: Long): Double { // Leibniz series: 1 - 1/3 + 1/5 - 1/7 + 1/9 - .... ~ pi / 4
+fun calculatePiWithCoroutines() = runBlocking {
+    // approximate pi up to 9 digits: up to 1_000_000_000 terms
+    val termCount = 1_000_000_000L
+    val iterations = 10
+    val interval = termCount / iterations
+
+    val intermediateResults = DoubleArray(iterations)
+    val coroutines = Array(iterations) {
+        launch { intermediateResults[it] = sumLeibniz(it * interval, (it + 1) * interval - 1) }
+    }
+    coroutines.forEach { it.join() }
+    val pi = intermediateResults.sum() * 4
+
+    println(pi) // (correct value of pi to 16 digits: 3.1415926535897932)
+}
+
+suspend fun sumLeibniz(start: Long, end: Long): Double { // Leibniz series: 1 - 1/3 + 1/5 - 1/7 + 1/9 - .... ~ pi / 4
 
     logger.info("{} .. {}", start, end)
 
     var result = 0.0
     for (factor in start..end) {
         result += (if (factor % 2 == 0L) 1.0 else -1.0) / (1 + 2 * factor)
+        if (factor % 100_000 == 0L) yield()
     }
     return result
 }
 
-fun warmUp( ){
+fun warmUp() {
 
     repeat(5) {
-        sumLeibniz(0, 1_000_000)
+        runBlocking { sumLeibniz(0, 1_000_000) }
     }
 
 }
