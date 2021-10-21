cb6e5c34fce93b7e5a5c9cb443aa42dd440d63c2
Vasco Veloso <vveloso@gmail.com>
2021-10-21 19:57:26 +0200

    Exercise 6 got a task description.

--- src/main/kotlin/coroutines/workshop/exercise6/thread-affinity.kt
+++ src/main/kotlin/coroutines/workshop/exercise6/thread-affinity.kt
@@ -6,6 +6,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 
+/*
+  Run the program as is and note the thread name that is part of the logger's output.
+  Experiment with the different Dispatchers to see how the output differs.
+  Pay particular attention to the output of the Unconfined dispatcher.
+ */
+
 fun main() = runBlocking {
     for(i in 0..10) {
         launch {
