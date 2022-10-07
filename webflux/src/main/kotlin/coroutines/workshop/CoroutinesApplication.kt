095913cc6eea8b38d81f25b2cb7a1c7c86874a71
Vasco (V.) Correia Veloso <vasco.correia.veloso@ing.com>
2022-10-07 17:47:27 +0200

    Webflux demo skeleton.

---
+++ webflux/src/main/kotlin/coroutines/workshop/CoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package coroutines.workshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoroutinesApplication>(*args)
+}
---
+++ webflux/src/test/kotlin/coroutines/workshop/CoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package coroutines.workshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutinesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
